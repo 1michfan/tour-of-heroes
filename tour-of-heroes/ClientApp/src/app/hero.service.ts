317cc1a6ba13cd22db1b5bcf7bfe7a4292e49a58
Matt Oosterbaan <moosterbaan@bdo.com>
2021-08-27 16:10:54 -0400

    update heroes

--- tour-of-heroes/ClientApp/src/app/hero.service.ts
+++ tour-of-heroes/ClientApp/src/app/hero.service.ts
@@ -33,6 +33,17 @@ export class HeroService {
     );
   }
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    )
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
